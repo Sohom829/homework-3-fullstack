b54562d9720da22f952a292718e373bc1f50afe6
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2022-09-04 22:38:28 +0600

    Some backend work

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const chalk = require('chalk');
+const express = require("express");
+const chalk = require("chalk");
 let app = express();
 var PORT = 3000;
-if(PORT !== 3000) {
-    throw new TypeError('PORT IS NOT 3000');
+if (PORT !== 3000) {
+  throw new TypeError("PORT IS NOT 3000");
 }
 app.listen(PORT, () => {
-    console.log(chalk.cyan('SERVER STARTED'));
-})
-app.get('/', (req, res) => {
-    res.send('<h1>PROJECTS<h1>');
-    res.send('<h3>/password-gen<h3>')
-})
-app.get('/password-gen', (req, res) => {
-    res.sendFile(__dirname + "/Password-Generator/web/main.html");
-    
-})
-app.use(express.static(__dirname + '/Password-Generator/web'));
+  console.log(chalk.cyan("SERVER STARTED " + chalk.greenBright('http://localhost:3000')));
+});
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/main/index.html");
+});
+app.get("/home", (req, res) => {
+  res.sendFile(__dirname + "/main/index.html");
+});
+app.get("/password-gen", (req, res) => {
+  res.sendFile(__dirname + "/Password-Generator/web/main.html");
+});
+app.use(express.static(__dirname + "/Password-Generator/web"));
+app.use(express.static(__dirname + "/main"));
+app.use(express.static(__dirname + "/not-found"));
+app.use((req, res) => {
+    res.sendFile(__dirname + "/not-found/main.html");
+});
